f1c5af10c17649969cd7dfdf7fb292f771d2df75
jamie <jamie.moreland@kcl.ac.uk>
2017-11-09 18:11:24 +0000

    Add basic repository classes

---
+++ Clustri.Core/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Clustri.Core.Entities
+{
+    public class User
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "weight")]
+        public double Weight { get; set; }
+    }
+}
